a2ef52226a94ba58708f4d1b4f016340fc51b6f9
Chen <chen.yang.bytes@gmail.com>
2019-12-21 00:07:52 -0500

    rename threadpool to lean; implement partially finish

--- threadpool.h
+++ threadpool.h
@@ -2,12 +2,14 @@
 #define LEAN_THREADPOOL_H
 
 #include <future>       // packaged_task
+#include <functional>
 #include <thread>
 #include <mutex>
 #include <condition_variable>
 #include <cstdint>
 #include <queue>
 #include <vector>
+#include <memory>
 
 
 
@@ -16,15 +18,27 @@ namespace lean {
     template<typename T>
     class ThreadPool {
     public:
+        ThreadPool<T>();
+        ThreadPool<T>(std::uint64_t n_workers);
+        ~ThreadPool<T>();
+
+        ThreadPool<T>& add_task(std::packaged_task<T()>&&);
+
+        ThreadPool<T>& run();
+        std::vector<T> get_results();
     private:
         mutable std::mutex m_mu;
         mutable std::condition_variable m_cv;
-        std::uint64_t m_cnt;
         bool m_done;
-        std::queue<T> m_job_q;
+        std::queue<std::function<T()>> m_job_q;
+        std::vector<std::unique_ptr<std::thread>> m_workers;
+        std::vector<std::future<std::vector<T>>> m_result_getters;
+
+        std::vector<T> m_work()
     };  // class ThreadPool
   } // v1
 } // lean
 
+#include "threadpool_impl.hpp"
 
 #endif
---
+++ threadpool_impl.hpp
@@ -0,0 +1,67 @@
+namespace lean {
+	inline namespace v1 {
+		template<typename T>
+		ThreadPool<T>::ThreadPool()
+			m_done(true)
+		{
+			auto n_workers = std::thread::hardware_concurrency();
+			m_workers = std::vector<std::unique_ptr<std::thread>>(n_workers, nullptr);
+		}
+
+		template<typename T>
+		ThreadPool<T>::ThreadPool(std::uint64_t n_workers)
+			m_done(true), m_workers(n_workers, nullptr)
+		{
+		}
+
+		template<typename T>
+		ThreadPool<T>::~ThreadPool()
+		{
+			std::lock_guard<std::mutex> lk(m_mu);
+			m_done = true;
+			lk.unlock();
+			for (auto& uptr_worker : m_workers) {
+				upter_worker->join();
+			}
+		}
+
+		template<typename T>
+		std::thread ThreadPool<T>::run()
+		{
+			for (size_t i = 0; i < m_workers.size(); ++i) {
+				std::packaged_task<T()> worker(this->m_work);
+				m_result_getters.emplace_back(worker.get_future());
+				m_workers[i] = std::make_unique(std::move(worker));
+			}
+		}
+
+		template<typename T>
+		std::vector<T> ThreadPool<T>::get_results()
+		{
+			std::vector<T> results;
+			for (auto& result_getter : m_result_getters) {
+				auto res = result_getter.get();
+				results.insert(results.end(), res.begin(), res.end());
+			}
+			return results;
+		}
+
+		template<typename T>
+		std::vector<T> ThreadPool<T>::m_work()
+		{
+			std::vector<T> results;
+			std::unique_lock<std::mutex> lk(m_mu);
+			while (!m_done) {
+				m_cv.wait(lk, [this]() {return m_job_q.size() > 0; });
+				
+				std::function<T()> task = std::move(m_job_q.front());
+				m_job_q.pop_front();
+				lk.unlock();
+
+				auto res = task();
+				results.push_back(res);
+			}
+			return results;
+		}
+	}
+}
